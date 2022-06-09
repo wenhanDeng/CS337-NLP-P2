fe6789f9772554bdab42490437f882572a4fe232
jackfreilich <44552935+jackfreilich@users.noreply.github.com>
2022-06-09 19:38:39 -0400

    Add files via upload
    lactose, conversions, and scaling

---
+++ lactose_free.py
@@ -0,0 +1,37 @@
+from web_scraping import *
+
+url = 'https://www.allrecipes.com/recipe/24074/alysias-basic-meat-lasagna/'
+
+def lactose_free(ingredients):
+	lactose_free_ingredients = []
+	for i in ingredients[1]:
+		category = ingredients[1][i]
+		if category == 'Dairy, Eggs and Milk' and 'eggs' not in i:
+			if 'milk' in i and 'almond' not in i and 'soy' not in i and 'coconut' not in i and 'oat' not in i:
+				print(i, ' became ', 'dairy-free milk of choice')
+				lactose_free_ingredients.append('dairy-free milk of choice')
+			elif 'cream' in i and 'almond' not in i and 'soy' not in i and 'coconut' not in i and 'oat' not in i and 'ice' not in i:
+				print(i, ' became ', 'dairy-free cream of choice')
+				lactose_free_ingredients.append('dairy-free cream of choice')
+			elif 'yogurt' in i and 'almond' not in i and 'soy' not in i and 'coconut' not in i and 'oat' not in i:
+				print(i, ' became ', 'dairy-free yogurt of choice')
+				lactose_free_ingredients.append('dairy-free yogurt of choice')
+			elif 'cheese' in i:
+				print(i, ' became ', 'dairy-free ' + i)
+				lactose_free_ingredients.append('dairy-free ' + i)
+			else:
+				print(i, ' became ', 'dairy-free alternative for ', i, ' of choice')
+				lactose_free_ingredients.append('dairy-free alternative for ', i, ' of choice')
+		elif 'butter' in i and 'almond' not in i and 'soy' not in i and 'coconut' not in i and 'nut' not in i:
+			print('nut butter or coconut butter')
+			lactose_free_ingredients.append('nut butter or coconut butter')
+		else:
+			lactose_free_ingredients.append(i)
+	print(ingredients[1])
+	print(lactose_free_ingredients)
+	return lactose_free_ingredients
+		
+
+measurements = get_quant(url)
+ingredients = get_ingredients(url)
+lactose_free(ingredients)
